a989b088abdc98c4d9b8f46df78214361697909d
wildcard329 <enkiduwildman@gmail.com>
2020-10-07 13:11:59 -0700

    starting social

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -26,7 +26,6 @@ def earliest_ancestor(ancestors, starting_node):
             if q.size() == 0:
                 if v == starting_node:
                     return -1
-                print(p)
                 return v
             continue
 
@@ -34,7 +33,3 @@ def earliest_ancestor(ancestors, starting_node):
             if node is not None:
                 q.enqueue(p + [node])
     
-    print(paths)
-
-test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-earliest_ancestor(test_ancestors, 6)
